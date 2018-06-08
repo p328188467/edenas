4ed16a2dae0dcf0298f7248dd556b46c4539a7df
panxinghao <panxinghao@migu.com>
2018-06-08 16:57:51 +0800

    update

--- src/fashion_minst/data_utils.py
+++ src/fashion_minst/data_utils.py
@@ -4,8 +4,9 @@ import pickle as pickle
 import numpy as np
 import tensorflow as tf
 
+import src.fashion_minst.utils.mnist_reader as mnist_reader
 
-def _read_data(data_path, train_files):
+def _read_data(data_path):
   """Reads CIFAR-10 format data. Always returns NHWC format.
 
   Returns:
@@ -13,23 +14,44 @@ def _read_data(data_path, train_files):
     labels: np tensor of size [N]
   """
   images, labels = [], []
-  for file_name in train_files:
-    print (file_name)
-    full_name = os.path.join(data_path, file_name)
-    with open(full_name,'rb') as finp:
-      data = pickle.load(finp,encoding='bytes')
-      print(data)
-      batch_images = data[b'data'].astype(np.float32) / 255.0
-      batch_labels = np.array(data[b'labels'], dtype=np.int32)
-      images.append(batch_images)
-      labels.append(batch_labels)
+ 
+  batch_images, batch_labels = mnist_reader.load_mnist(data_path, kind='train')
+    #X_test, y_test = mnist_reader.load_mnist('data/fashion', kind='t10k')
+    #data = pickle.load(finp,encoding='bytes')
+  print(batch_images)
+    #batch_images = data[b'data'].astype(np.float32) / 255.0
+    #batch_labels = np.array(data[b'labels'], dtype=np.int32)
+  images.append(batch_images)
+  labels.append(batch_labels)
   images = np.concatenate(images, axis=0)
   labels = np.concatenate(labels, axis=0)
-  images = np.reshape(images, [-1, 3, 32, 32])
+  images = np.reshape(images, [-1, 1, 28, 28])
   images = np.transpose(images, [0, 2, 3, 1])
 
   return images, labels
 
+def _read_data_test(data_path):
+  """Reads CIFAR-10 format data. Always returns NHWC format.
+
+  Returns:
+    images: np tensor of size [N, H, W, C]
+    labels: np tensor of size [N]
+  """
+  images, labels = [], []
+  batch_images, batch_labels = mnist_reader.load_mnist(data_path, kind='t10k')
+    #X_test, y_test = mnist_reader.load_mnist('data/fashion', kind='t10k')
+    #data = pickle.load(finp,encoding='bytes')
+  print(batch_images)
+    #batch_images = data[b'data'].astype(np.float32) / 255.0
+    #batch_labels = np.array(data[b'labels'], dtype=np.int32)
+  images.append(batch_images)
+  labels.append(batch_labels)
+  images = np.concatenate(images, axis=0)
+  labels = np.concatenate(labels, axis=0)
+  images = np.reshape(images, [-1, 1, 28, 28])
+  images = np.transpose(images, [0, 2, 3, 1])
+
+  return images, labels
 
 def read_data(data_path, num_valids=5000):
   print ("-" * 80)
@@ -37,17 +59,8 @@ def read_data(data_path, num_valids=5000):
 
   images, labels = {}, {}
 
-  train_files = [
-    "data_batch_1",
-    "data_batch_2",
-    "data_batch_3",
-    "data_batch_4",
-    "data_batch_5",
-  ]
-  test_file = [
-    "test_batch",
-  ]
-  images["train"], labels["train"] = _read_data(data_path, train_files)
+  
+  images["train"], labels["train"] = _read_data(data_path)
 
   if num_valids:
     images["valid"] = images["train"][-num_valids:]
@@ -58,7 +71,7 @@ def read_data(data_path, num_valids=5000):
   else:
     images["valid"], labels["valid"] = None, None
 
-  images["test"], labels["test"] = _read_data(data_path, test_file)
+  images["test"], labels["test"] = _read_data_test(data_path)
 
   print ("Prepropcess: [subtract mean], [divide std]")
   mean = np.mean(images["train"], axis=(0, 1, 2), keepdims=True)
